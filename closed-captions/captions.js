217b5069aa5198f1ecb58e245b5d164a2666764a
Demi Louise <63409781+demilouise@users.noreply.github.com>
2020-05-13 23:57:50 +0200

    Versie 3

--- closed-captions/captions.js
+++ closed-captions/captions.js
@@ -1,109 +1,89 @@
-captions[0] = ['3.8','8.1'];
-captions[1] = ['8.1','10'];
-captions[2] = ['10.8','12.5'];
-	//And blood-black nothingness began to spin
-captions[3] = ['12.5','15.6'];
-captions[4] = ['17.369','20'];
-captions[5] = ['20.1','23.3'];
-	//Fuck off, skin-job!
-captions[6] = ['23.3','25.2'];
-	
-captions[7] = ['26.8','31.6'];
-captions[8] = ['32.7','33'];
-captions[9] = ['33.2','34'];
-	//Have you ever been in an institution?
-captions[10] = ['34','36'];
-captions[11] = ['36','36.3'];
-captions[12] = ['36.5','37.3'];
-	//Do they keep you in a cell?
-captions[13] = ['37.3','38.6'];
-captions[14] = ['38.6','38.9'];
-captions[15] = ['39.1','39.9'];
-	//When you're not performing your duties, do they keep you in a little box?
-captions[16] = ['39.9','42.8'];
-captions[17] = ['42.8','43.1'];
-captions[18] = ['43.3','44.1'];
-	//Interlinked
-captions[19] = ['44','44.5'];
-captions[20] = ['45','45.7'];
-	//What's it like to hold the hand of someone you love?
-captions[21] = ['45.7','47.6'];
-captions[22] = ['47.6','48.1'];
-captions[23] = ['48.3','49'];
-	//Did they teach you how to feel, finger to finger?
-captions[24] = ['49.1','50.9'];
-captions[25] = ['50.9','51.4'];
-captions[26] = ['51.6','52.3'];
-	//Do you long for having your heart interlinked?
-captions[27] = ['52.3','54'];
-captions[28] = ['54','54.5'];
-captions[29] = ['54.7','55.4'];
-	//Do you dream about being interlinked?
-captions[30] = ['55.4','57.2'];
-captions[31] = ['57.2','58'];
-	//What's it like to hold your child in your arms?
-captions[32] = ['58','59.8'];
-captions[33] = ['59.8','60.3'];
-captions[34] = ['60.3','61'];
-	//Do you feel that there's a part of you that's missing?
-captions[35] = ['61','62.7'];
-captions[36] = ['62.7','63.2'];
-captions[37] = ['63.2','63.9'];
-	//Within cells interlinked.
-captions[38] = ['63.9','65.2'];
-captions[39] = ['65.2','66.6'];
-	//Why don't you say that three times: "within cells interlinked".
-captions[40] = ['66.6','69.1'];
-
-captions[41] = ['69.1','70.6'];
-captions[42] = ['70.6','72.2'];
-captions[43] = ['72.2','74.2'];
-	// We're done.
-captions[44] = ['77.1','78.7'];
-captions[45] = ['80','81.738'];
-captions[46] = ['82.2','84.3'];
-captions[47] = ['85.4','87'];
-
+captions[0] = ['3.8','6'];
+captions[1] = ['6.6','7.3'];
+captions[2] = ['7.3','8'];
+captions[3] = ['8.1','10'];
+captions[4] = ['10.8','12.3'];
+captions[5] = ['12.3','15.6']; //And blood-black nothingness began to spin
+captions[6] = ['17.369','19.8'];
+captions[7] = ['19.8','23.3'];
+captions[8] = ['23.3','25.2']; //Fuck off, skin-job!
+captions[9] = ['26.8','31.4'];
+captions[10] = ['32.6','33.1'];
+captions[11] = ['33.1','33.8'];
+captions[12] = ['33.8','35.5']; //Have you ever been in an institution?
+captions[13] = ['35.5','36.3'];
+captions[14] = ['36.3','36.8'];
+captions[15] = ['36.8','38.2']; //Do they keep you in a cell?
+captions[16] = ['38.2','39.1'];
+captions[17] = ['39.1','39.2'];
+captions[18] = ['39.2','42']; //When you're not performing your duties, do they keep you in a little box?
+captions[19] = ['42','43.1'];
+captions[20] = ['43.1','43.9'];
+captions[21] = ['43.9','44.7']; // Interlinked
+captions[22] = ['44.6','45.3'];
+captions[23] = ['45.3','47.1']; // What's it like to hold the hand of someone you love?
+captions[24] = ['47.2','48.2'];
+captions[25] = ['48.2','48.7'];
+captions[26] = ['48.7','50.5']; // Did they teach you how to feel, finger to finger?
+captions[27] = ['50.5','51.6'];
+captions[28] = ['51.6','52'];
+captions[29] = ['52','53.5']; // Do you long for having your heart interlinked?
+captions[30] = ['53.5','54.5'];
+captions[31] = ['54.5','55.2'];
+captions[32] = ['55.2','57.2']; // Do you dream about being interlinked?
+captions[33] = ['57.2','58'];
+captions[34] = ['58','59.6']; // What's it like to hold your child in your arms?
+captions[35] = ['59.6','60.4'];
+captions[36] = ['60.4','61'];
+captions[37] = ['61','62.7']; // Do you feel that there's a part of you that's missing?
+captions[38] = ['62.7','63.2'];
+captions[39] = ['63.2','63.9'];
+captions[40] = ['63.9','65.2']; // Within cells interlinked.
+captions[41] = ['65.2','66.6'];
+captions[42] = ['66.6','68.9']; // Why don't you say that three times: "within cells interlinked".
+captions[43] = ['68.9','70.6'];
+captions[44] = ['70.6','72.2'];
+captions[45] = ['72.2','74.2'];
+captions[46] = ['77.1','78.7']; // We're done.
+captions[47] = ['80','81.738'];
+captions[48] = ['82.2','84.3'];
+captions[49] = ['85.4','86.2']; // Thank you, sir
 //end manual labour
-captions[48] = ['87.68','88.18'];
-captions[49] = ['88.38','88.88'];
-// Have you ever been in an institution. Cells
-captions[50] = ['88.88','91.24'];
-captions[51] = ['91.34','91.84'];
-captions[52] = ['91.98','95.52'];
-// -Cells
-captions[53] = ['95.52','96.02'];
-captions[54] = ['96.12','100.2'];
-captions[55] = ['100.3','102.4'];
-// -Interlinked
-captions[56] = ['102.64','103.3'];
-captions[57] = ['103.3','104.38'];
-captions[58] = ['104.54','105.42'];
-// Dreadfully
-captions[59] = ['105.42','105.92'];
-captions[60] = ['106','106.5'];
-captions[61] = ['106.64','109.28'];
-// -Dreadfully
-captions[62] = ['109.28','109.8'];
-captions[63] = ['109.8','112.4'];
-captions[64] = ['112.4','113'];
-// Dreadfully distinct
-captions[65] = ['113','114.06'];
-captions[66] = ['114.12','114.98'];
-captions[67] = ['115.02','115.56'];
-// -Dark
-captions[68] = ['115.56','116.26'];
-captions[69] = ['116.26','117.4'];
-captions[70] = ['117.4','118.44'];
-// Withing one stem
-captions[71] = ['118.44','120'];
-captions[72] = ['120.46','121.28'];
-captions[73] = ['121.4','122.66'];
-// -And dreadfully distinct
-captions[74] = ['122.66','123.86'];
-captions[75] = ['123.98','124.92'];
-captions[76] = ['124.92','125.52'];
-// A tall white fountain played
-captions[77] = ['125.52','127.28'];
-captions[78] = ['128.18','130.14'];
-captions[79] = ['132.58','135'];
+captions[50] = ['87.3','87.8']; // Cells
+captions[51] = ['87.8','88.3']; // Cells
+captions[52] = ['88.4','89.9']; // Have you ever been in an institution.
+captions[53] = ['89.9','90.5']; // Cells
+captions[54] = ['90.8','91.3']; // Cells
+captions[55] = ['91.3','94']; // When you're
+captions[56] = ['94.2','95']; // Cells
+captions[57] = ['95.2','95.8']; // Cells
+captions[58] = ['95.8','96.4']; // Inter
+captions[59] = ['96.4','96.9']; // Inter
+captions[60] = ['97','99.7']; // What is it
+captions[61] = ['99.7','101']; // Inter
+captions[62] = ['102.5','103']; // Inter
+captions[63] = ['103','104']; // Within
+captions[64] = ['104','105']; // Within
+captions[65] = ['105','105.5']; // Dreadfully
+captions[66] = ['105.5','106']; // Dreadfully
+captions[67] = ['106','107.8']; // What's it like to be filled with dread?
+captions[68] = ['108','108.6']; // Dreadfully
+captions[69] = ['108.5','109.3']; // Dreadfully
+captions[70] = ['109.3','111']; // Do you like being separated from other people?
+captions[71] = ['111','112.2']; // Distinct
+captions[72] = ['112.2','112.7']; // Distinct
+captions[73] = ['112.6','113.5']; // Dreadfully distinct
+captions[74] = ['113.5','114.8']; // Dreadfully distinct
+captions[75] = ['115.02','115.56']; // Dark
+captions[76] = ['115.56','116.15']; // Dark
+captions[77] = ['116.15','117.4']; // Within cells interlinked
+captions[78] = ['117.4','118.44']; // Within cells interlinked
+captions[79] = ['118.44','120']; // Withing one stem
+captions[80] = ['120.46','121.28']; // Withing one stem
+captions[81] = ['121.4','122.66']; // And dreadfully distinct
+captions[82] = ['122.66','123.86']; // And dreadfully distinct
+captions[83] = ['123.98','124.92']; // Against the dark
+captions[84] = ['124.92','125.52']; // Against the dark
+captions[85] = ['125.52','127.28']; // A tall white fountain played
+captions[86] = ['128.18','130.14']; // A tall white fountain played
+captions[87] = ['132.58','135']; // You're not even close to baseline
